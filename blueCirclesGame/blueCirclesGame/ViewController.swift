812afe755c8c96ec49c7d42d7f88435dfc406819
TonyJem <a.jemanov@gmail.com>
2021-01-16 21:47:06 +0200

    Remove "currentChild" variable due it is not neccessary

--- blueCirclesGame/blueCirclesGame/ViewController.swift
+++ blueCirclesGame/blueCirclesGame/ViewController.swift
@@ -10,8 +10,6 @@ class ViewController: UIViewController {
     @IBOutlet private weak var circle6: UIView!
     @IBOutlet var circles: [UIView]!
     
-    var currentChild = UIView()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         for circle in circles {
@@ -21,43 +19,31 @@ class ViewController: UIViewController {
     
     @IBAction private func pan1Action(_ gesture: UIPanGestureRecognizer) {
         moveView(for: gesture)
-        print("1 moved")
-        currentChild = circle1
         hideIfNeeded(justMovedView: circle1)
     }
     
     @IBAction private func pan2Action(_ gesture: UIPanGestureRecognizer) {
         moveView(for: gesture)
-        print("2 moved")
-        currentChild = circle2
         hideIfNeeded(justMovedView: circle2)
     }
     
     @IBAction private func pan3Action(_ gesture: UIPanGestureRecognizer) {
         moveView(for: gesture)
-        print("3 moved")
-        currentChild = circle3
         hideIfNeeded(justMovedView: circle3)
     }
     
     @IBAction private func pan4Action(_ gesture: UIPanGestureRecognizer) {
         moveView(for: gesture)
-        print("4 moved")
-        currentChild = circle4
         hideIfNeeded(justMovedView: circle4)
     }
     
     @IBAction func pan5Action(_ gesture: UIPanGestureRecognizer) {
         moveView(for: gesture)
-        print("5 moved")
-        currentChild = circle5
         hideIfNeeded(justMovedView: circle5)
     }
     
     @IBAction func pan6Action(_ gesture: UIPanGestureRecognizer) {
         moveView(for: gesture)
-        print("6 moved")
-        currentChild = circle6
         hideIfNeeded(justMovedView: circle6)
     }
     
