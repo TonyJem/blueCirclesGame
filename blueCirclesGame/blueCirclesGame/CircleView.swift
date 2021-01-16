dbad8562e534ace676611e40afcaf3759af05af2
TonyJem <a.jemanov@gmail.com>
2021-01-16 23:48:34 +0200

    Refactor methods in "CircleView" class

--- blueCirclesGame/blueCirclesGame/CircleView.swift
+++ blueCirclesGame/blueCirclesGame/CircleView.swift
@@ -11,15 +11,17 @@ class CircleView: UIView {
         let commonArea = otherCircle.area + area
         let newRadius = (commonArea/CGFloat.pi).squareRoot()
         radius = newRadius
+        setSize(with: radius)
+        setBackroundColor(with: .blue)
     }
     
-    private func setSize() {
+    func setSize(with radius: CGFloat) {
         self.frame.size.width = radius * 2
         self.frame.size.height = radius * 2
         self.layer.cornerRadius = radius
     }
     
-    private func setColor() {
-        self.backgroundColor = UIColor.blue
+    func setBackroundColor(with color: UIColor) {
+        self.backgroundColor = color
     }
 }
