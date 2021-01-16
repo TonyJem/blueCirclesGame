7d85f985d8782fa797984e0b57e159835e2bd4dc
TonyJem <a.jemanov@gmail.com>
2021-01-16 23:43:57 +0200

    Update "CircleView" class with it's properties and methods

--- blueCirclesGame/blueCirclesGame/CircleView.swift
+++ blueCirclesGame/blueCirclesGame/CircleView.swift
@@ -1,5 +1,25 @@
 import UIKit
 
 class CircleView: UIView {
-
+    var radius: CGFloat = 100
+    
+    var area: CGFloat {
+        return pow(radius, 2) * CGFloat.pi
+    }
+    
+    func absorb(otherCircle: CircleView) {
+        let commonArea = otherCircle.area + area
+        let newRadius = (commonArea/CGFloat.pi).squareRoot()
+        radius = newRadius
+    }
+    
+    private func setSize() {
+        self.frame.size.width = radius * 2
+        self.frame.size.height = radius * 2
+        self.layer.cornerRadius = radius
+    }
+    
+    private func setColor() {
+        self.backgroundColor = UIColor.blue
+    }
 }
