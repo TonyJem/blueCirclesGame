2665e895ccb93bc63ef9c9e07cfa50badfd06905
TonyJem <a.jemanov@gmail.com>
2021-01-16 17:55:35 +0200

    POC move 3 views in screen with UIPanGestureRecognizer

--- blueCirclesGame/blueCirclesGame/ViewController.swift
+++ blueCirclesGame/blueCirclesGame/ViewController.swift
@@ -1,9 +1,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
+    @IBAction private func orangePanAction(_ gesture: UIPanGestureRecognizer) {
+        moveView(for: gesture)
+    }
+    
+    @IBAction private func greenPanAction(_ gesture: UIPanGestureRecognizer) {
+        moveView(for: gesture)
+    }
+    
+    
+    @IBAction private func bluePanAction(_ gesture: UIPanGestureRecognizer) {
+        moveView(for: gesture)
+    }
+    
+    
+    private func moveView(for gesture: UIPanGestureRecognizer) {
+        let gestureTranslation = gesture.translation(in: view)
+        guard let gestureView = gesture.view else { return }
+        
+        gestureView.center = CGPoint(
+            x: gestureView.center.x + gestureTranslation.x,
+            y: gestureView.center.y + gestureTranslation.y
+        )
+        
+        gesture.setTranslation(.zero, in: view)
+        guard gesture.state == .ended else { return }
+    }
 }
